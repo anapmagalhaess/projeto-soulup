7e65b3d5d9519739a2d3304b6450a13a1da72c52
anapmagalhaess <bruclaramagalhaes2015@gmail.com>
2026-09-07 21:34:40 -0300

    feat: Reajuste do footer para acolher novas informações.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,10 @@
 export default function Footer() {
     return(
         <footer>
-            <p className="footer__p">&copy; 2026 SOUL UP. Todos os direitos reservados.</p>
-        </footer>
+            <div className="footer wrap">
+                <p className="footer-brand">soul<span className="brand-badge">UP</span></p>
+                <p className="footer-p">soul up · atitude vira recompensa · © 2026</p>
+            </div>
+    </footer>
     );
 }
