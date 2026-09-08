92236f77c7c10be7196d0a0cc6d6cf56b84d6be0
anapmagalhaess <bruclaramagalhaes2015@gmail.com>
2026-09-08 13:34:55 -0300

    feat: Criação do componente reutilizavel 'tag' com uso de prop!

---
+++ src/components/Tag/Tag.tsx
@@ -0,0 +1,32 @@
+type TagProps = {
+    texto: string;
+    className?: string;
+};
+
+export default function Tag({
+    texto,
+    className = "",
+}: TagProps) {
+    return (
+    <span
+        className={`
+        inline-flex
+        items-center
+        gap-[6px]
+        rounded-full
+        bg-[#FFE9E4]
+        px-[14px]
+        py-[6px]
+        [font-family:var(--fonte-titulo)]
+        text-[12.5px]
+        font-semibold
+        tracking-[0.02em]
+        text-[var(--cor-coral-escuro)]
+        rotate-[-1.5deg]
+        ${className}
+        `}
+    >
+        <strong>✦ {texto}</strong>
+    </span>
+    );
+}
